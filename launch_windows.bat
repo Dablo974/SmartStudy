301ee509883c9f57565c266f70cf6906aa68d320
gaetan.maronne <gaetan.maronne@epita.fr>
2025-06-12 16:16:06 +0200

    better windows launch file

---
+++ launch_windows.bat
@@ -0,0 +1,43 @@
+@echo off
+chcp 65001 >nul
+
+echo ------------------------------------------------------------
+echo Si nodejs ou npm ne sont pas installé sur votre machine : https://nodejs.org/
+echo ------------------------------------------------------------
+echo:
+echo:
+
+:: Vérification de l'installation de Node.js
+where node >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Erreur : Node.js n'est pas installé.
+    pause
+    exit /b 1
+)
+
+:: Vérification de l'installation de npm
+where npm >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Erreur : npm n'est pas installé.
+    pause
+    exit /b 1
+)
+
+echo Installation des dépendances...
+call npm install
+if %errorlevel% neq 0 (
+    echo Erreur lors de l'installation des dépendances.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Construction du projet...
+call npm run start
+if %errorlevel% neq 0 (
+    echo Erreur lors de la construction du projet.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Le projet a été construit avec succès.
+pause
